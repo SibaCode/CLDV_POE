27f475ab88cd718031dd8a67619db5c66df000a9
SibahleMvubu <smvubu@iqbusiness.net>
2025-05-07 12:42:13 +0200

    can search

--- EventEaseWebsite/obj/Debug/net8.0/EventEaseWebsite.AssemblyInfo.cs
+++ EventEaseWebsite/obj/Debug/net8.0/EventEaseWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85823e6e82f0d319fab281294c59497966a0ab03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f9c7450ffe57763e39c8c37c26e30a80692f45")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
