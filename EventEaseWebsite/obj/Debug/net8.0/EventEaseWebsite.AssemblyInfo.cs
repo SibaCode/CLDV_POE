4bb8f7154338e7389533ab073234f7a8fbcddbe5
SibahleMvubu <smvubu@iqbusiness.net>
2025-05-07 19:05:36 +0200

    can save and display images

--- EventEaseWebsite/obj/Debug/net8.0/EventEaseWebsite.AssemblyInfo.cs
+++ EventEaseWebsite/obj/Debug/net8.0/EventEaseWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a2e7f3839ef0db205f18fe911b75142badfb58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7985650ff45961c5c207ed46dbd881d2eff6d66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
